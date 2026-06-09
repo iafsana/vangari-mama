c0000fe8dc99d6d3a0e4d7b5cb261ca157838d54
ashraf452b <ashraf452b@gmail.com>
2026-06-09 10:00:42 +0600

    Flask to Django

---
+++ Vangari_Mama/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Vangari_Mama project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vangari_Mama.settings')
+
+application = get_asgi_application()
